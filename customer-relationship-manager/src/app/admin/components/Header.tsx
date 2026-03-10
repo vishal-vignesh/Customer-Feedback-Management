924ac5526757c62b3b6235aee76dc8a4c69ce4d8
Sitharth varsan S <sitharthvarsan@gmail.com>
2026-03-10 19:59:17 +0530

    jenkins test8

--- customer-relationship-manager/src/app/admin/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-white shadow px-6 py-4">
-      <h2 className="text-lg font-semibold">Admin Dashboard</h2>
-    </header>
-  );
-}
